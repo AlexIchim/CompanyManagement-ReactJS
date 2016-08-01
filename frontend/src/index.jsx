f280592ff6f1a20e448ba0fcf4316374d544d668
Lupacescu Eduard <lupacescueduard@yahoo.com>
2016-08-01 16:06:43 +0300

    Init frontend project.

---
+++ frontend/src/index.jsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import  {Router, Route,hashHistory, IndexRoute} from 'react-router';
+import * as ReactDOM from 'react-dom';
+import App from './components/App';
+import ConcertsList from './components/concertslist/ConcertsList.jsx';
+import './assets/less/index.less';
+import Dashboard from './components/dashboard/Dashboard.jsx';
+
+const Routes = () => {
+
+    return (
+        <Router history={hashHistory}>
+            <Route path="/" component={App}>
+                <IndexRoute component={Dashboard}/>
+                <Route path="ConcertsList" component={ConcertsList}>
+                </Route>
+            </Route>
+        </Router>
+    )
+
+};
+
+
+ReactDOM.render(<Routes/>, document.getElementById('root'));
