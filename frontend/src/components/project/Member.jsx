a98cf8ed08d7b35a5efef186864152795965b24f
cami.bodnar <camelia.bodnar@evozon.com>
2016-08-02 17:32:17 +0300

    Display the get methods from backend (offices, departments, employees, projects)

---
+++ frontend/src/components/project/Member.jsx
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router';
+import * as $ from 'jquery';
+import configs from '../helpers/calls'
+
+const Tr = (props) => {
+
+    return(
+        <tr>
+            <td>{props.node.Name} </td>
+            <td>{props.node.Role}</td>
+            <td>{props.node.Allocation}</td>
+            <td><Link to="#"> Edit allocation | </Link>
+                <Link to="#"> Remove </Link></td>
+        </tr>
+    )
+
+
+}
+
+export default class Member extends React.Component{
+
+
+    constructor(){
+        super();
+        this.state ={
+            members: []
+        }
+    }
+
+    componentWillMount(){
+        $.ajax({
+            method: 'GET',
+            url: configs.baseUrl + 'api/project/getAllDepartmentProjects?inputInfo=' + this.props.routeParams.departmentId,
+            success: function(data){
+                this.setState({
+                    members: data
+                })
+
+            }.bind(this)
+        })
+    }
+
+    render(){
+
+        const items = this.state.members.map( (element, index) => {
+            return(
+                <Tr
+                    node = {element}
+                    key = {index}
+                />
+            )
+
+        });
+
+        return(
+            <div>
+                <h1>{this.props.routeParams.projectName + ' Members'}  </h1>
+                <button className="btn btn-xs btn-info" > <span className="glyphicon glyphicon-plus-sign"></span> Assign employee </button>
+                <table className="table table-condensed" id="table1">
+                    <thead>
+                    <tr>
+                        <th className="col-md-2">Name</th>
+                        <th className="col-md-2">Role</th>
+                        <th className="col-md-2">Allocation</th>
+                        <th className="col-md-2">Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {items}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+
+
+
+}
---
+++ frontend/src/components/project/Project.jsx
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router';
+import * as $ from 'jquery';
+import configs from '../helpers/calls'
+
+const Tr = (props) => {
+
+    const linkMembers = "department/project/" + props.node.Id  + '/' + props.node.Name + "/members";
+
+    return(
+        <tr>
+            <td>{props.node.Name} </td>
+            <td>{props.node.EmployeesNumber}</td>
+            <td>{props.node.Duration}</td>
+            <td>{props.node.Status}</td>
+            <td><Link to=""> Edit | </Link>
+                <Link to={linkMembers}> View Members | </Link>
+                <Link to="#"> Remove </Link></td>
+        </tr>
+    )
+
+
+}
+
+export default class Project extends React.Component{
+
+
+    constructor(){
+        super();
+        this.state ={
+            proj: []
+        }
+    }
+
+    componentWillMount(){
+        $.ajax({
+            method: 'GET',
+            url: configs.baseUrl + 'api/department/getAllDepartmentProjects?inputInfo=' + this.props.routeParams.departmentId,
+            success: function(data){
+                this.setState({
+                    proj: data
+                })
+
+            }.bind(this)
+        })
+    }
+
+    render(){
+
+        const items = this.state.proj.map( (element, index) => {
+            return(
+                <Tr
+                    node = {element}
+                    key = {index}
+
+                />
+            )
+
+        });
+
+        return(
+            <div>
+                <h1>{this.props.routeParams.departmentName + ' Projects'}  </h1>
+                <button className="btn btn-xs btn-info" > <span className="glyphicon glyphicon-plus-sign"></span> Add new project </button>
+                <table className="table table-condensed" id="table1">
+                    <thead>
+                    <tr>
+                        <th className="col-md-2">Name</th>
+                        <th className="col-md-2">Team members</th>
+                        <th className="col-md-2">Duration</th>
+                        <th className="col-md-2">Status</th>
+                        <th className="col-md-2">Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {items}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+
+
+
+}
