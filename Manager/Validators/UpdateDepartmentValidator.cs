2aece36cea48d2f81c8228fd33ed705ef6a973c9
Hidi Paul <paul.hidi@evozon.com>
2016-08-02 12:32:08 +0300

    Defined Validators for Services (Not yet implemented)

---
+++ Manager/Validators/UpdateDepartmentValidator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using Manager.InputInfoModels;
+
+namespace Manager.Validators
+{
+    public class UpdateDepartmentValidator: AbstractValidator<UpdateDepartmentInputInfo>
+    {
+        public UpdateDepartmentValidator()
+        {
+            RuleFor(m => m.Id)
+                .NotNull()
+                .GreaterThan(0);
+
+            RuleFor(m => m.Name).NotEmpty();
+            RuleFor(m => m.Name.Length).LessThanOrEqualTo(30);
+
+            RuleFor(m => m.DepartmentManagerId)
+                .NotNull()
+                .GreaterThan(0);
+        }
+    }
+}
---
+++ Manager/Validators/UpdateOfficeValidator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using Manager.InputInfoModels;
+
+namespace Manager.Validators
+{
+    public class UpdateOfficeValidator: AbstractValidator<UpdateOfficeInputInfo>
+    {
+        public UpdateOfficeValidator()
+        {
+            RuleFor(m => m.Id)
+                .NotNull()
+                .GreaterThan(0);
+
+            RuleFor(m => m.Name).NotEmpty();
+            RuleFor(m => m.Name.Length).LessThanOrEqualTo(30);
+
+            RuleFor(m => m.Address).NotEmpty();
+            RuleFor(m => m.Address.Length).LessThanOrEqualTo(100);
+
+            RuleFor(m => m.Phone).NotEmpty();
+            RuleFor(m => m.Phone.Length)
+                .GreaterThanOrEqualTo(10)
+                .LessThanOrEqualTo(14);
+
+            RuleFor(m => m.Image).NotEmpty();
+        }
+    }
+}
