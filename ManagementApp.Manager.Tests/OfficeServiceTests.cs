eb0704cd63a5914d3c30b22b849c72397e9655fc
Alexandru Ichim <daniel.ichim@evozon.com>
2016-08-02 14:24:59 +0300

    Add tests for Project/Office functionalities.

---
+++ ManagementApp.Manager.Tests/OfficeServiceTests.cs
@@ -0,0 +1,293 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using Contracts;
+using Domain.Models;
+using Manager;
+using Manager.InfoModels;
+using Manager.InputInfoModels;
+using Manager.Services;
+using Moq;
+using NUnit.Framework;
+
+namespace ManagementApp.Manager.Tests
+{
+    [TestFixture]
+    public class OfficeServiceTests
+    {
+        private OfficeService _officeService;
+        private Mock<IOfficeRepository> _officeRepositoryMock;
+        private Mock<IMapper> _mapperMock;
+
+        [SetUp]
+        public void PerTestSetup()
+        {
+            _officeRepositoryMock = new Mock<IOfficeRepository>();
+            _mapperMock = new Mock<IMapper>();
+            _officeService = new OfficeService(_mapperMock.Object, _officeRepositoryMock.Object);
+        }
+
+        [Test]
+        public void GetAll_ReturnsAListOfOffices()
+        {
+            //Arrange
+            var offices = new List<Office>
+            {
+                CreateOffice("Cluj-Napoca", "Motilor nr 16", "4016161251", new byte[] { 0x20, 0x20, 0x20}),
+                CreateOffice("London", "HighWay 13 Nr", "401616125411", new byte[] { 0x20, 0x20, 0x20})
+            };
+            var officesInfo = new List<OfficeInfo>
+            {
+                CreateOfficeInfo(1,"Cluj-Napoca", "Motilor nr 16", "4016161251", new byte[] { 0x20, 0x20, 0x20}),
+                CreateOfficeInfo(2,"London", "HighWay 13 Nr", "401616125411", new byte[] { 0x20, 0x20, 0x20})
+            };
+
+            _officeRepositoryMock.Setup(m => m.GetAll()).Returns(offices);
+            _mapperMock.Setup(m => m.Map<IEnumerable<OfficeInfo>>(offices)).Returns(officesInfo);
+
+            //Act
+            var result = _officeService.GetAll();
+
+            //Assert
+            Assert.AreEqual(2, result.Count());
+        }
+
+        [Test]
+        public void GetAll_CallsGetAllFromRepository()
+        {
+            //Arrange
+
+            //Act
+            _officeService.GetAll();
+
+            //Assert
+            _officeRepositoryMock.Verify(x => x.GetAll(), Times.Once);
+        }
+
+        [Test]
+        public void Add_CallsAddFromRepository()
+        {
+            //Arrange
+            var officeInputInfo = new AddOfficeInputInfo { Name = "Cluj-Napoca", Address = "Motilor nr12", Phone = "0740179200", Image = new byte[3] };
+            var office = new Office { Name = "Cluj-Napoca", Address = "Motilor nr12", Phone = "0740179200", Image = new byte[3] };
+
+            _mapperMock.Setup(m => m.Map<Office>(officeInputInfo)).Returns(office);
+            _officeRepositoryMock.Setup(m => m.Add(office));
+
+            //Act
+            _officeService.Add(officeInputInfo);
+
+            //Assert
+            _officeRepositoryMock.Verify(x => x.Add(office), Times.Once);
+        }
+
+        [Test]
+        public void Add_ReturnsSuccessfulMessage()
+        {
+            //Arrange
+            var officeInputInfo = new AddOfficeInputInfo { Name = "Cluj-Napoca", Address = "Motilor nr12", Phone = "0740179200", Image = new byte[3] };
+            var office = CreateOffice("Cluj-Napoca", "Motilor nr 16", "4016161251", new byte[] { 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20 });
+
+            _mapperMock.Setup(m => m.Map<Office>(officeInputInfo)).Returns(office);
+            _officeRepositoryMock.Setup(m => m.Add(office));
+
+            //Act
+            var result = _officeService.Add(officeInputInfo);
+
+            //Assert
+            Assert.IsTrue(result.Success);
+            Assert.AreEqual(Messages.SuccessfullyAddedOffice, result.Message);
+        }
+
+
+        [Test]
+        public void Update_ReturnsSuccessfulMessage_WhenOfficeExists()
+        {
+            //Arrange
+            var officeInputInfo = new UpdateOfficeInputInfo { Id = 1, Name = "Cluj-Napoca", Address = "Motilor nr12", Phone = "0740179200", Image = new byte[3] };
+            var office = new Office { Name = "Cluj-Napoca", Address = "Motilor nr12", Phone = "0740179200", Image = new byte[3] };
+
+            _officeRepositoryMock.Setup(m => m.GetById(officeInputInfo.Id)).Returns(office);
+
+            //Act
+            var result = _officeService.Update(officeInputInfo);
+
+            //Assert
+            Assert.IsTrue(result.Success);
+            Assert.AreEqual(Messages.SuccessfullyUpdatedOffice, result.Message);
+        }
+
+
+        [Test]
+        public void Update_CallsGetByIdFromRepository_WhenOfficeExists()
+        {
+            //Arrange
+            var officeInputInfo = new UpdateOfficeInputInfo { Id = 1, Name = "Cluj-Napoca", Address = "Motilor nr12", Phone = "0740179200", Image = new byte[3] };
+            var office = new Office { Name = "Cluj-Napoca", Address = "Motilor nr12", Phone = "0740179200", Image = new byte[3] };
+
+            _officeRepositoryMock.Setup(m => m.GetById(officeInputInfo.Id)).Returns(office);
+
+            //Act
+            _officeService.Update(officeInputInfo);
+
+            //Assert
+            _officeRepositoryMock.Verify(x => x.GetById(officeInputInfo.Id), Times.Once);
+        }
+
+        [Test]
+        public void Update_CallsSaveFromRepository_WhenOfficeExists()
+        {
+            //Arrange
+            var officeInputInfo = new UpdateOfficeInputInfo { Id = 1, Name = "Cluj-Napoca", Address = "Motilor nr12", Phone = "0740179200", Image = new byte[3] };
+            var office = new Office { Name = "Cluj-Napoca", Address = "Motilor nr12", Phone = "0740179200", Image = new byte[3] };
+
+            _officeRepositoryMock.Setup(m => m.GetById(officeInputInfo.Id)).Returns(office);
+
+            //Act
+            _officeService.Update(officeInputInfo);
+
+            //Assert
+            _officeRepositoryMock.Verify(x => x.Save(), Times.Once);
+        }
+
+        [Test]
+        public void Update_ReturnsErrorMessage_WhenOfficeDoesNotExists()
+        {
+            //Arrange
+            var officeInputInfo = new UpdateOfficeInputInfo { Id = 1, Name = "Cluj-Napoca", Address = "Motilor nr12", Phone = "0740179200", Image = new byte[3] };
+
+            _officeRepositoryMock.Setup(m => m.GetById(officeInputInfo.Id)).Returns((Office)null);
+
+            //Act
+            var result = _officeService.Update(officeInputInfo);
+
+            //Assert
+            Assert.IsFalse(result.Success);
+            Assert.AreEqual(Messages.ErrorWhileUpdatingOffice_InvalidId, result.Message);
+        }
+
+        [Test]
+        public void Update_DoesNotCallSaveFromRepository_WhenOfficeDoesNotExists()
+        {
+            //Arrange
+            var officeInputInfo = new UpdateOfficeInputInfo { Id = 1, Name = "Cluj-Napoca", Address = "Motilor nr12", Phone = "0740179200", Image = new byte[3] };
+
+            _officeRepositoryMock.Setup(m => m.GetById(officeInputInfo.Id)).Returns((Office)null);
+
+            //Act
+            _officeService.Update(officeInputInfo);
+
+            //Assert
+            _officeRepositoryMock.Verify(x => x.Save(), Times.Never);
+        }
+
+        [Test]
+        public void Add_ReturnsErrorMessageOnInvalidNameInput()
+        {
+            //Arrange
+            var officeInputInfoNameEmpty = new AddOfficeInputInfo { Name = "", Address = "Motilor nr12", Phone = "0740179200", Image = new byte[3] };
+            var officeNameEmpty = new Office() {Name = "Cluj-Napoca", Address = "Motilor nr12", Phone = "0740179200", Image = new byte[3] };
+
+            var officeInputInfoNameTooLong = new AddOfficeInputInfo { Name = new string('*', 101), Address = "Motilor nr12", Phone = "0740179200", Image = new byte[3] };
+            var officeNameTooLong = new Office() { Name = "", Address = "Motilor nr12", Phone = "0740179200", Image = new byte[3] };
+
+            _mapperMock.Setup(m => m.Map<Office>(officeInputInfoNameEmpty)).Returns(officeNameEmpty);
+            _mapperMock.Setup(m => m.Map<Office>(officeInputInfoNameTooLong)).Returns(officeNameTooLong);
+            _officeRepositoryMock.Setup(m => m.Add(officeNameEmpty));
+            _officeRepositoryMock.Setup(m => m.Add(officeNameTooLong));
+
+            //Act
+            var resultNameEmpty = _officeService.Add(officeInputInfoNameEmpty);
+            var resultNameTooLong = _officeService.Add(officeInputInfoNameTooLong);
+
+
+            //Assert
+            Assert.IsFalse(resultNameEmpty.Success);
+            Assert.AreEqual(Messages.ErrorWhileAddingOffice_EmptyName, resultNameEmpty.Message);
+            Assert.IsFalse(resultNameTooLong.Success);
+            Assert.AreEqual(Messages.ErrorWhileAddingOffice_NameTooLong, resultNameTooLong.Message);
+        }
+
+        [Test]
+        public void Add_ReturnsErrorMessageOnInvalidAddressInput()
+        {
+            //Arrange
+            var officeInputInfoAddressEmpty = new AddOfficeInputInfo { Name = "Tokio", Address = "", Phone = "0740179200", Image = new byte[3] };
+            var officeAddressEmpty = new Office() { Name = "Cluj-Napoca", Address = "", Phone = "0740179200", Image = new byte[3] };
+
+            var officeInputInfoAddressTooLong = new AddOfficeInputInfo { Name = "Tokio", Address = new string('*', 301), Phone = "0740179200", Image = new byte[3] };
+            var officeAddressTooLong = new Office() { Name = "Cluj-Napoca", Address = new string('*', 301), Phone = "0740179200", Image = new byte[3] };
+
+            _mapperMock.Setup(m => m.Map<Office>(officeInputInfoAddressEmpty)).Returns(officeAddressEmpty);
+            _mapperMock.Setup(m => m.Map<Office>(officeInputInfoAddressTooLong)).Returns(officeAddressTooLong);
+            _officeRepositoryMock.Setup(m => m.Add(officeAddressEmpty));
+            _officeRepositoryMock.Setup(m => m.Add(officeAddressTooLong));
+
+            //Act
+            var resultAddressEmpty = _officeService.Add(officeInputInfoAddressEmpty);
+            var resultAddressTooLong = _officeService.Add(officeInputInfoAddressTooLong);
+
+
+            //Assert
+            Assert.IsFalse(resultAddressEmpty.Success);
+            Assert.AreEqual(Messages.ErrorWhileAddingOffice_EmptyAddress, resultAddressEmpty.Message);
+            Assert.IsFalse(resultAddressTooLong.Success);
+            Assert.AreEqual(Messages.ErrorWhileAddingOffice_AddressTooLong, resultAddressTooLong.Message);
+        }
+
+        [Test]
+        public void Add_ReturnsErrorMessageOnInvalidPhoneInput()
+        {
+            //Arrange
+            var officeInputInfoPhoneEmpty = new AddOfficeInputInfo { Name = "Tokio", Address = "Motilor nr1", Phone = "", Image = new byte[3] };
+            var officePhoneEmpty = new Office() { Name = "Cluj-Napoca", Address = "", Phone = "0740179200", Image = new byte[3] };
+
+            var officeInputInfoPhoneTooLong = new AddOfficeInputInfo { Name = "Tokio", Address = "Motilor nr1", Phone = new string('*', 21), Image = new byte[3] };
+            var officePhoneTooLong = new Office() { Name = "Cluj-Napoca", Address = "Motilor nr1", Phone = "0740179200", Image = new byte[3] };
+
+            _mapperMock.Setup(m => m.Map<Office>(officeInputInfoPhoneEmpty)).Returns(officePhoneEmpty);
+            _mapperMock.Setup(m => m.Map<Office>(officeInputInfoPhoneTooLong)).Returns(officePhoneTooLong);
+            _officeRepositoryMock.Setup(m => m.Add(officePhoneEmpty));
+            _officeRepositoryMock.Setup(m => m.Add(officePhoneTooLong));
+
+            //Act
+            var resultPhoneEmpty = _officeService.Add(officeInputInfoPhoneEmpty);
+            var resultPhoneTooLong = _officeService.Add(officeInputInfoPhoneTooLong);
+
+
+            //Assert
+            Assert.IsFalse(resultPhoneEmpty.Success);
+            Assert.AreEqual(Messages.ErrorWhileAddingOffice_EmptyPhone, resultPhoneEmpty.Message);
+            Assert.IsFalse(resultPhoneTooLong.Success);
+            Assert.AreEqual(Messages.ErrorWhileAddingOffice_PhoneTooLong, resultPhoneTooLong.Message);
+        }
+
+
+
+
+        private Office CreateOffice(string name, string address, string phone, byte[] image)
+        {
+            var office = new Office
+            {
+                Name = name,
+                Address = address,
+                Phone = phone,
+                Image = image
+            };
+
+            return office;
+        }
+
+        private OfficeInfo CreateOfficeInfo(int id, string name, string address, string phone, byte[] image)
+        {
+            return new OfficeInfo
+            {
+                Id = id,
+                Name = name,
+                Address = address,
+                Phone = phone,
+                Image = image
+            };
+        }
+    }
+}
---
+++ ManagementApp.Manager.Tests/ProjectServiceTests.cs
@@ -0,0 +1,272 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using Contracts;
+using Domain.Models;
+using Manager;
+using Manager.InfoModels;
+using Manager.InputInfoModels;
+using Manager.Services;
+using Moq;
+using NUnit.Framework;
+
+namespace ManagementApp.Manager.Tests
+{
+    [TestFixture]
+    public class ProjectServiceTests
+    {
+        private ProjectService _projectService;
+        private Mock<IProjectRepository> _projectRepositoryMock;
+        private Mock<IDepartmentRepository> _departmentRepositoryMock;
+        private Mock<IMapper> _mapperMock;
+
+        [SetUp]
+        public void PerTestSetup()
+        {
+            _projectRepositoryMock = new Mock<IProjectRepository>();
+            _departmentRepositoryMock = new Mock<IDepartmentRepository>();
+            _mapperMock = new Mock<IMapper>();
+            _projectService = new ProjectService(_mapperMock.Object, _projectRepositoryMock.Object, _departmentRepositoryMock.Object);
+        }
+
+
+        [Test]
+        public void GetAll_ReturnsAListOfProjectsIfDepartmentExists()
+        {
+            //Arrange
+            var departments = new List<Department>
+            {
+                CreateDepartment(1, "Javascript"),
+                CreateDepartment(2, "Java")
+            };
+            var departmentsInfo = new List<DepartmentInfo>
+            {
+               CreateDepartmentInfo(1, 1,"Jaavascript"),
+            };
+
+            var projects = new List<Project>
+            {
+                CreateProject("Alex Project", "In progress", 6, 1),
+                CreateProject("Robi Project", "Done", 8, 1),
+            };
+            var projectsInfo = new List<ProjectInfo>
+            {
+               CreateProjectInfo(1,"Alex Project", "In progress", 6, 1),
+               CreateProjectInfo(2,"Robi Project", "Done", 8, 1 )
+            };
+
+            _departmentRepositoryMock.Setup(m => m.GetAll()).Returns(departments);
+            _mapperMock.Setup(m => m.Map<IEnumerable<DepartmentInfo>>(departments)).Returns(departmentsInfo);
+
+            _projectRepositoryMock.Setup(m => m.GetAll()).Returns(projects);
+            _mapperMock.Setup(m => m.Map<IEnumerable<ProjectInfo>>(projects)).Returns(projectsInfo);
+
+
+            //Act
+            var result = _projectService.GetAll();
+
+            //Assert
+            Assert.AreEqual(2, result.Count());
+        }
+
+        [Test]
+        public void Add_CallsAddFromRepository()
+        {
+            //Arrange
+            var projectInputInfo = new AddProjectInputInfo { Name = "Project", Status = "In progress", Duration = 6, DepartmentId = 1 };
+            var project = new Project { Name = "Project", Status = "In progress", Duration = 6, DepartmentId = 1 };
+
+            var department = new Department { OfficeId = 1, Name = "Java" };
+            _departmentRepositoryMock.Setup(m => m.GetById(projectInputInfo.DepartmentId)).Returns(department);
+
+
+            _mapperMock.Setup(m => m.Map<Project>(projectInputInfo)).Returns(project);
+            _projectRepositoryMock.Setup(m => m.Add(project));
+
+            //Act
+            _projectService.Add(projectInputInfo);
+
+            //Assert
+            _projectRepositoryMock.Verify(x => x.Add(project), Times.Once);
+        }
+
+        [Test]
+        public void Add_ReturnsSuccessfulMessage()
+        {
+            //Arrange
+            var projectInputInfo = new AddProjectInputInfo { Name = "Project", Status = "In progress", Duration = 6, DepartmentId = 1 };
+            var project = new Project { Name = "Project", Status = "In progress", Duration = 6, DepartmentId = 1 };
+
+            _mapperMock.Setup(m => m.Map<Project>(projectInputInfo)).Returns(project);
+            _projectRepositoryMock.Setup(m => m.Add(project));
+
+            var department = new Department {OfficeId = 1, Name = "Java"};
+            _departmentRepositoryMock.Setup(m => m.GetById(projectInputInfo.DepartmentId)).Returns(department);
+
+            //Act
+            var result = _projectService.Add(projectInputInfo);
+
+            //Assert
+            Assert.IsTrue(result.Success);
+            Assert.AreEqual(Messages.SuccessfullyAddedProject, result.Message);
+        }
+
+        [Test]
+        public void Update_ReturnsSuccessfulMessage_WhenProjectExists()
+        {
+            //Arrange
+            var projectInputInfo = new UpdateProjectInputInfo { Id=1,  Name = "Project", Status = "In progress", Duration = 6, DepartmentId = 1 };
+            var project = new Project { Name = "Project", Status = "In progress", Duration = 6, DepartmentId = 1 };
+
+            _mapperMock.Setup(m => m.Map<Project>(projectInputInfo)).Returns(project);
+            _projectRepositoryMock.Setup(m => m.GetById(projectInputInfo.Id)).Returns(project);
+
+            var department = new Department {OfficeId = 1, Name = "Java"};
+            _departmentRepositoryMock.Setup(m => m.GetById(projectInputInfo.DepartmentId.Value)).Returns(department);
+
+            //Act
+            var result = _projectService.Update(projectInputInfo);
+
+            //Assert
+            Assert.IsTrue(result.Success);
+            Assert.AreEqual(Messages.SuccessfullyUpdatedProject, result.Message);
+        }
+
+        [Test]
+        public void Update_ReturnsErrorMessage_WhenProjectNot()
+        {
+            //Arrange
+            var projectInputInfo = new UpdateProjectInputInfo { Id = 1, Name = "Project", Status = "In progress", Duration = 6, DepartmentId = 1 };
+            var project = new Project { Name = "Project", Status = "In progress", Duration = 6, DepartmentId = 1 };
+
+            var department = new Department { OfficeId = 1, Name = "Java" };
+            _departmentRepositoryMock.Setup(m => m.GetById(projectInputInfo.DepartmentId.Value)).Returns(department);
+
+            _mapperMock.Setup(m => m.Map<Project>(projectInputInfo)).Returns(project);
+            _projectRepositoryMock.Setup(m => m.GetById(projectInputInfo.Id)).Returns((Project)null);
+
+            //Act
+            var result = _projectService.Update(projectInputInfo);
+
+            //Assert
+            Assert.IsFalse(result.Success);
+            Assert.AreEqual(Messages.ErrorWhileUpdatingProject_InvalidId, result.Message);
+        }
+
+
+        [Test]
+        public void Add_ReturnsErrorMessageOnInvalidNameInput()
+        {
+            //Arrange
+            var department = new Department {OfficeId = 1, Name = "Java"};
+
+            
+            var projectInputInfoNameEmpty = new AddProjectInputInfo { Name = "", Status = "In progress", Duration = 6, DepartmentId = 1 };
+            var projectNameEmpty = new Project { Name = "", Status = "In progress", Duration = 6, DepartmentId = 1 };
+
+            var projectInputInfoNameTooLong = new AddProjectInputInfo { Name = new string('$', 101), Status = "In progress", Duration = 6, DepartmentId = 1 };
+            var projectNameTooLong = new Project { Name = new string('$', 101), Status = "In progress", Duration = 6, DepartmentId = 1 };
+
+            _departmentRepositoryMock.Setup(m => m.GetById(projectInputInfoNameEmpty.DepartmentId)).Returns(department);
+
+
+            _mapperMock.Setup(m => m.Map<Project>(projectInputInfoNameEmpty)).Returns(projectNameEmpty);
+            _mapperMock.Setup(m => m.Map<Project>(projectInputInfoNameTooLong)).Returns(projectNameTooLong);
+            _projectRepositoryMock.Setup(m => m.Add(projectNameEmpty));
+            _projectRepositoryMock.Setup(m => m.Add(projectNameTooLong));
+
+            //Act
+            var resultNameEmpty = _projectService.Add(projectInputInfoNameEmpty);
+            var resultNameTooLong = _projectService.Add(projectInputInfoNameTooLong);
+
+
+            //Assert
+            Assert.IsFalse(resultNameEmpty.Success);
+            Assert.AreEqual(Messages.ErrorWhileAddingProject + Messages.ProjectNameEmpty, resultNameEmpty.Message);
+            Assert.IsFalse(resultNameTooLong.Success);
+            Assert.AreEqual(Messages.ErrorWhileAddingProject + Messages.ProjectNameTooLong, resultNameTooLong.Message);
+        }
+
+        [Test]
+        public void Add_ReturnsErrorMessageOnInvalidStatusInput()
+        {
+            //Arrange
+            var department = new Department { OfficeId = 1, Name = "Java" };
+
+            var projectInputInfoStatusEmpty = new AddProjectInputInfo { Name = "New Project", Status = "", Duration = 6, DepartmentId = 1 };
+            var projectStatusEmpty = new Project { Name = "New Project", Status = "", Duration = 6, DepartmentId = 1 };
+
+            var projectInputInfoStatusTooLong = new AddProjectInputInfo { Name = "New Project", Status = new string('$', 101), Duration = 6, DepartmentId = 1 };
+            var projectStatusTooLong = new Project { Name = "New project", Status = new string('$', 101), Duration = 6, DepartmentId = 1 };
+
+            _departmentRepositoryMock.Setup(m => m.GetById(projectInputInfoStatusEmpty.DepartmentId)).Returns(department);
+
+
+            _mapperMock.Setup(m => m.Map<Project>(projectInputInfoStatusEmpty)).Returns(projectStatusEmpty);
+            _mapperMock.Setup(m => m.Map<Project>(projectInputInfoStatusTooLong)).Returns(projectStatusTooLong);
+            _projectRepositoryMock.Setup(m => m.Add(projectStatusEmpty));
+            _projectRepositoryMock.Setup(m => m.Add(projectStatusTooLong));
+
+            //Act
+            var resultStatusEmpty = _projectService.Add(projectInputInfoStatusEmpty);
+            var resultStatusTooLong = _projectService.Add(projectInputInfoStatusTooLong);
+
+
+            //Assert
+            Assert.IsFalse(resultStatusEmpty.Success);
+            Assert.AreEqual(Messages.ErrorWhileAddingProject + Messages.ProjectStatusEmpty, resultStatusEmpty.Message);
+            Assert.IsFalse(resultStatusTooLong.Success);
+            Assert.AreEqual(Messages.ErrorWhileAddingProject + Messages.ProjectStatusTooLong, resultStatusTooLong.Message);
+        }
+
+
+
+        private Project CreateProject(string name, string status, int? duration, int? departmentId)
+        {
+            var project = new Project
+            {
+                Name = name,
+                Status = status,
+                Duration = duration,
+                DepartmentId = departmentId
+            };
+
+            return project;
+        }
+
+        private ProjectInfo CreateProjectInfo(int id, string name, string status, int? duration, int? departmentId)
+        {
+            return new ProjectInfo
+            {
+                Id = id,
+                Name = name,
+                Status = status,
+                Duration = duration,
+                DepartmentId = departmentId
+            };
+        }
+
+
+
+        private Department CreateDepartment(int officeId, string name)
+        {
+            var department = new Department
+            {
+                OfficeId = officeId,
+                Name = name,
+            };
+
+            return department;
+        }
+
+        private DepartmentInfo CreateDepartmentInfo(int id, int officeId, string name)
+        {
+            return new DepartmentInfo
+            {
+                Id = id,
+                OfficeId = officeId,
+                Name = name,
+            };
+        }
+    }
+}
