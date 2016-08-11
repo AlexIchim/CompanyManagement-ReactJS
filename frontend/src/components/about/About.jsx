411698e155d22e92bc08a676dfcdad6c34f8af5c
Alexandru Ichim <daniel.ichim@evozon.com>
2016-08-11 16:20:01 +0300

    Modify about page.

--- frontend/src/components/about/About.jsx
+++ frontend/src/components/about/About.jsx
@@ -5,14 +5,22 @@ export default class About extends Component {
         return (
             <div>
                 <h1>HR Project - Team2</h1>
-                <br/>
-                <h3>Created By</h3>
-                <ul>
-                    <li>Camelia Trif</li>
-                    <li>Alexandru Ichim</li>
-                    <li>Robert Varadi</li>
-                    <li>Arnold Beiland</li>
-                </ul>
+                <div>
+                    <div className="col-md-6">
+                        <h3>Created By</h3>
+                        <ul>
+                            <li>Camelia Trif</li>
+                            <li>Alexandru Ichim</li>
+                            <li>Robert Varadi</li>
+                            <li>Arnold Beiland</li>
+                        </ul>
+                        <br/>
+                        <span className="text-danger">Cei patru fanatici...</span>
+                    </div>
+                    <div className="col-md-6 pull-down">
+                        <img src="assets/images/logo-evozon.png"/>
+                    </div>
+                </div>
             </div>
         );
     }
